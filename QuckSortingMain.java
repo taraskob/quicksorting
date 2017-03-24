aa404e3cea971df57a25b55c42f5de2b14995c3b
Taras Kobevko <taraskob@yahoo.com>
2017-03-24 15:49:32 +0200

    mode

--- QuckSortingMain.java
+++ QuckSortingMain.java
@@ -2,17 +2,17 @@ import java.util.Random;
 
 class QuckSortingMain {
     public static void main(String[] args) {
-        QuickSorting.getSortedArray(generateArray());
+        QuickSorting.getSortedArray(generateArray(21));
     }
 
-    static String[] generateArray() {
+    static String[] generateArray(int rsize) {
         String abc = "qwertyuioplkjhgfdsazxcvbnmMNBVCXZASDFGHJKLLPOIUYTREWQ";
         Random rnd = new Random();
-        int size = rnd.nextInt(21) + 3;
+        int size = rnd.nextInt(rsize) + 12;
         String[] array = new String[size];
         for (int i = 0; i < size; i++) {
             String result = "";
-            int length = rnd.nextInt(7) + 1;
+            int length = rnd.nextInt(12) + 1;
             for (int j = 0; j < length; j++) {
                 result += abc.charAt(rnd.nextInt(abc.length()));
             }
--- QuickSortingTest.java
+++ QuickSortingTest.java
@@ -1,14 +1,21 @@
-import java.util.Formatter;
+import java.io.*;
+import java.text.ParseException;
 import java.util.Random;
 
-import static java.lang.System.arraycopy;
-
 class QuickSortingTest {
+    static String fileName = "test_array.txt";
+    static File file = new File(fileName);
+
     public static void main(String[] args) {
-        String[] array = QuckSortingMain.generateArray();
-        show(array, "Before sorting");
-        String[] sortedArray = QuickSorting.getSortedArray(array);
-        show(sortedArray, "After sorting");
+        try {
+            String[] array = readTestArray(fileName);
+            show(array, "Before sorting");
+            show(QuickSorting.getSortedArray(array), "After sorting");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     static void show(String[] sortedArray, String str) {
@@ -17,5 +24,45 @@ class QuickSortingTest {
             System.out.print(sortedArray[i] + " ");
         }
         System.out.println();
+    }
+
+    static void testArrayExists(String fileName) throws IOException {
+
+        if (!file.exists()) {
+            String abc = "qwertyuioplkjhgfdsazxcvbnmMNBVCXZASDFGHJKLLPOIUYTREWQ";
+            Random rnd = new Random();
+            String[] array = new String[2112];
+            FileWriter writer = new FileWriter(fileName);
+            for (int i = 0; i < array.length; i++) {
+                String result = String.valueOf(i + 1) + '.';
+                int elementLength = rnd.nextInt(12) + 1;
+                for (int j = 0; j < elementLength; j++) {
+                    result += abc.charAt(rnd.nextInt(abc.length()));
+                }
+                writer.write(result + "\r\n");
+            }
+            writer.close();
+        }
+    }
+
+    static String[] readTestArray(String filename) throws IllegalAccessException, ParseException {
+        StringBuilder line = new StringBuilder();
+
+        try {
+            testArrayExists(filename);
+            BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
+            try {
+                String s;
+                while ((s = in.readLine()) != null) {
+                    line.append(s);
+                    line.append("\n");
+                }
+            } finally {
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line.toString().split("\n");
     }
 }
